1996e5a7c911dc490bdd29b547e24cd2907d8b6c
Sathvik <sathvikachanta003@gmail.com>
2022-06-26 20:19:27 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Sorting from './components/Sorting';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+    <div className = "App" >
+        <Sorting />
     </div>
-  );
+    );
 }
 
 export default App;
